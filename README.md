678e2e04c117480a203ea84e89eb6b5ab20b4dea
Geoffrey Beausire <g.beausire@criteo.com>
2018-03-22 15:51:25 +0100

    Application and configuration

--- README.md
+++ README.md
@@ -1,1 +1,49 @@
 # django-memcached-consul
+
+Django-memcached-consul helps use consul for memcached servers.
+It will query Consul Health API to get list of servers every time we use the cache.
+
+## Usage
+
+The configuration is done in the cache part of local_settings.py in the Django project.
+Django-memcached-consul use the Django memcached driver so all configuration related to the
+caching itself should be done like in django memcached.
+
+To use consul you need to use the specific Django-memcached-consul driver:
+`django-consul-memcached.memcached.MemcachedCache`
+
+And to provide informations about Consul and the required services:
+```
+CONSUL_HOST # The Consul API host
+CONSUL_PORT # The Consul API port
+CONSUL_SERVICE # The service of the memcached servers in Consul
+```
+It is possible and strongly recommended to use a cache for the consul, otherwise an Consul API call
+will be issued every time we use the cache.
+
+Django-memcached-consul use two layer of cache: the first layer should be short lived (`CONSUL_TTL`)
+and is used to cache the list of memcached servers and is retrieved everytime we use the
+django-consul-memcached driver. The second one is long lived (`CONSUL_TTL_ALT`) and retrieved when
+Consul API is unreachable. The cache used is named `consul-memcached` and should be preferably
+a filebased cache to be shared accross all workers.
+
+## Example
+
+```
+CACHES = {
+    'default': {
+        'BACKEND': 'django-consul-memcached.memcached.MemcachedCache',
+        'TIMEOUT': 60,
+        'CONSUL_TTL': 60,
+        # Alt cache will be used if consul is unreachable
+        'CONSUL_ALT_TTL': 3600,
+        'CONSUL_HOST': 'consul.organization.com',
+        'CONSUL_PORT': 8500,
+        'CONSUL_SERVICE': 'memcached-service',
+    },
+    'consul-memcached': {
+        'BACKEND': 'django.core.cache.backends.filebased.FileBasedCache',
+        'LOCATION': '/var/tmp/django_consul_cache',
+    }
+}
+```
---
+++ django-consul-memcached/memcached.py
@@ -0,0 +1,58 @@
+from django.core.cache import caches
+from django.core.cache.backends import memcached
+
+
+import consul
+import requests
+import logging
+
+
+def get_servers(params):
+    """Get the list of cache servers either from cache or directly from Consul."""
+    consul_cache = caches['consul-memcached']
+
+    cache_key = params["CONSUL_SERVICE"]
+    alt_cache_key = "alt:%s" % params["CONSUL_SERVICE"]
+    cached = consul_cache.get(cache_key)
+
+    if cached:
+        return cached
+    else:
+        try:
+            servers = get_servers_list_from_consul(params)
+            consul_cache.set(cache_key, servers, params["CONSUL_TTL"])
+            consul_cache.set(alt_cache_key, servers, params["CONSUL_ALT_TTL"])
+        except(requests.exceptions.ConnectionError) as e:
+            logging.warning("Cannot connect to Consul")
+            alt_cached = consul_cache.get(alt_cache_key)
+            if alt_cached:
+                return alt_cached
+            else:
+                servers = []
+        return servers
+
+
+def get_servers_list_from_consul(params):
+    """Get the list of cache servers using Consul."""
+    servers = []
+    consul_api = consul.Consul(host=params["CONSUL_HOST"],port=params["CONSUL_PORT"])
+    index, data = consul_api.health.service(service=params["CONSUL_SERVICE"], passing=True)
+    for node in data:
+        servers.append("%s:%s" % (node["Node"]["Node"],node["Service"]["Port"]))
+    return servers
+
+
+class BaseMemcachedCache(memcached.BaseMemcachedCache):
+    def __init__(self, server, params, library, value_not_found_exception):
+        server = get_servers(params)
+        memcached.BaseMemcachedCache.__init__(
+            self, server, params, library, value_not_found_exception)
+
+
+class MemcachedCache(memcached.MemcachedCache):
+    "An implementation of a cache binding using python-memcached"
+
+    def __init__(self, server, params):
+        import memcache
+        BaseMemcachedCache.__init__(
+            self, server, params, library=memcache, value_not_found_exception=ValueError)
