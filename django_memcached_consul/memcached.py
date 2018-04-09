18e7cb18d82669b356a78538c4336428d384e7a9
Geoffrey Beausire <g.beausire@criteo.com>
2018-04-09 17:21:45 +0200

    Added several metrics to have a better overview

--- django_memcached_consul/memcached.py
+++ django_memcached_consul/memcached.py
@@ -1,14 +1,31 @@
 from django.core.cache import caches
 from django.core.cache.backends import memcached
 
+from prometheus_client import Counter
+from prometheus_client import Gauge
 
 import consul
 import requests
 import logging
 
 
+dmc_requests_total = Counter(
+    'django_mc_request_total',
+    'Total requests to get list of servers')
+dmc_request_consul_total = Counter(
+    'django_mc_request_consul_total',
+    'Total requests to get list of servers from Consul')
+dmc_request_consul_failed_total = Counter(
+    'django_mc_request_consul_failed_total',
+    'Total failed requests to get list of servers from Consul')
+dmc_memcached_server_count = Gauge(
+    'django_mc_memcached_server_count',
+    'Number of memcached servers given by Consul')
+
+
 def get_servers(params):
     """Get the list of cache servers either from cache or directly from Consul."""
+    dmc_requests_total.inc()
     try:
         consul_cache = caches[params['CONSUL_CACHE']]
     except(KeyError):
@@ -26,11 +43,14 @@ def get_servers(params):
         return cached
     else:
         try:
+            dmc_request_consul_total.inc()
             servers = get_servers_list_from_consul(params)
             if consul_cache:
                 consul_cache.set(cache_key, servers, params["CONSUL_TTL"])
-                consul_cache.set(alt_cache_key, servers, params["CONSUL_ALT_TTL"])
+                consul_cache.set(alt_cache_key, servers,
+                                 params["CONSUL_ALT_TTL"])
         except(requests.exceptions.ConnectionError):
+            dmc_request_consul_failed_total.inc()
             logging.warning("Cannot connect to Consul")
             if consul_cache:
                 alt_cached = consul_cache.get(alt_cache_key)
@@ -44,16 +64,20 @@ def get_servers(params):
 def get_servers_list_from_consul(params):
     """Get the list of cache servers using Consul."""
     servers = []
-    consul_api = consul.Consul(host=params["CONSUL_HOST"], port=params["CONSUL_PORT"])
-    index, data = consul_api.health.service(service=params["CONSUL_SERVICE"], passing=True)
+    consul_api = consul.Consul(
+        host=params["CONSUL_HOST"], port=params["CONSUL_PORT"])
+    index, data = consul_api.health.service(
+        service=params["CONSUL_SERVICE"], passing=True)
     for node in data:
-        servers.append("%s:%s" % (node["Node"]["Node"], node["Service"]["Port"]))
+        servers.append("%s:%s" %
+                       (node["Node"]["Node"], node["Service"]["Port"]))
     return servers
 
 
 class BaseMemcachedCache(memcached.BaseMemcachedCache):
     def __init__(self, server, params, library, value_not_found_exception):
         server = get_servers(params)
+        dmc_memcached_server_count.set(len(server))
         memcached.BaseMemcachedCache.__init__(
             self, server, params, library, value_not_found_exception)
 
